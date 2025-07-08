43e26a2c1dd08169dafc152f87a4f832cd26d983
tanwee14 <8409tan@gmail.com>
2025-07-08 22:44:43 +0530

    Added new route

--- book_backend/routes/book.Routes.js
+++ book_backend/routes/book.Routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { addBook } = require("../controllers/book.controller");
+const { addBook,getBookById } = require("../controllers/book.controller");
 
 // POST /books/add
 router.post("/add", addBook);
+router.get("/:id",getBookById)
 
 module.exports = router;
