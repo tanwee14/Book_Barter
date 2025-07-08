35fc3322b8dc9730fbd9caa1bc9bde56afba1391
tanwee14 <8409tan@gmail.com>
2025-07-08 22:49:58 +0530

    Added user schema

---
+++ book_backend/models/user.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+
+  location: {
+    type: String,
+    required: true
+  },
+
+  profileImage: {
+    type: String,
+    default: "" // Optional profile picture
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
